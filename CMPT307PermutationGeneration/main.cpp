0a1242d8e6851d5e54030639e5725c996debc9f7
Kai Zhang <kwz@asb9700-f02.csil.sfu.ca>
2016-05-31 12:37:32 -0700

    Initial Commit

---
+++ CMPT307PermutationGeneration/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CMPT307PermutationGeneration
+//
+//  Created by Kai Zhang on 2016-05-31.
+//  Copyright (c) 2016 Kai Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
